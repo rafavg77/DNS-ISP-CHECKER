35aabd4dea1da6eb9abb4a6da2e82f245c013251
Tota77 <rafavg77@gmail.com>
2019-03-18 03:44:17 -0600

    Update script.py

--- script.py
+++ script.py
@@ -12,6 +12,8 @@ def query_dns(IP,NAME,CITY):
 	print('###########################################################')
 	print('Query to DNS %s with name %s from  %s' % (IP,NAME,CITY))
 	p = subprocess.Popen("dig @%s um.edu.mx A" % IP ,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	p = subprocess.Popen("nslookup -query=A um.edu.mx %s" % IP ,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	p = subprocess.Popen("nslookup -query=NS um.edu.mx %s" % IP ,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	#p = subprocess.Popen("dig @%s um.edu.mx A | sed -n '/QUESTION SECTION/,/Query time/p' | grep -v 'QUESTION SECTION' | grep -v 'Query time'" % IP, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True)
 	(output, err) = p.communicate()
 	p_status = p.wait()
@@ -24,7 +26,7 @@ def load_dns_isp():
 	    IP=x['ip']
 	    NAME=x['name']
 	    CITY=x['city']
-	    time.sleep(5)
+	    time.sleep(15)
 	    query_dns(IP,NAME,CITY)
 
 load_dns_isp()
